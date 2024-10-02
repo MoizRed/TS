c66dbb7dbeacf8c22be6aa7383f81958c6aab2c0
MoizRed <abdoureduniv2000@gmail.com>
2024-10-02 20:33:53 +0100

    this starting

---
+++ build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let myname;
+let meaningOfLife;
+let loading;
+let somthing;
+let a, b;
+myname = "moiz";
+meaningOfLife = 32;
+loading = false;
+const sum = (a, b) => {
+    return a + b;
+};
+let postID;
+postID = 89;
+let isActive;
+let re = /\w+/g;
---
+++ src/main.ts
@@ -0,0 +1,29 @@
+let myname: String;
+
+let meaningOfLife: number;
+
+let loading: boolean;
+
+let somthing: any | number;
+
+let a, b: number;
+
+myname = "moiz";
+
+meaningOfLife = 32;
+
+loading = false;
+
+const sum = (a: number, b: any) => {
+  return a + b;
+};
+
+let postID: string | number;
+
+postID = 89;
+
+let isActive: number | boolean;
+
+
+let re = /\w+/g 
+
