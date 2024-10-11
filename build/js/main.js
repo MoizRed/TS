f668e02e42f5a7641e03daf3508f1e6c02d90a64
MoizRed <abdoureduniv2000@gmail.com>
2024-10-11 22:45:48 +0100

    some changed

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,19 @@
 "use strict";
-//default value of type script
-function showdata(name = "guest", age, country = "algera") {
-    return `${name}  - ${age}  - ${country}`;
+// //default value of type script
+// function showdata(name: string | null = "guest", age? : number , country : string = "algera" ) : string{
+//     return `${name}  - ${age}  - ${country}`
+// }
+// console.log(showdata(null,987,"hi"))
+//Learn Typescript In Arabic 2022 - #10 - Function Rest Parameter 
+function addAll(...numbs) {
+    let result = 0;
+    //     for (let i = 0 ; i < numbs.length ; i ++ )
+    // {
+    //     result += numbs[i]
+    // }
+    numbs.forEach(nums => result += nums);
+    return result;
 }
-console.log(showdata(null, 987, "hi"));
+/// to convert boolean  into number just multiple it or add plus or nagetive sign
+console.log(addAll(1, 3, 6, 5, 5, -true * 1));
+//learn Typescript In Arabic 2022 - #11 - Type Annotations With Anonymous And Arrow Function
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 
 
-//default value of type script
-function showdata(name: string | null = "guest", age? : number , country : string = "algera" ) : string{
-    return `${name}  - ${age}  - ${country}`
-}
+// //default value of type script
+// function showdata(name: string | null = "guest", age? : number , country : string = "algera" ) : string{
+//     return `${name}  - ${age}  - ${country}`
+// }
 
-console.log(showdata(null,987,"hi"))
+// console.log(showdata(null,987,"hi"))
+
+
+//Learn Typescript In Arabic 2022 - #10 - Function Rest Parameter 
+
+
+function addAll(...numbs : number[]){
+
+   let result = 0;
+//     for (let i = 0 ; i < numbs.length ; i ++ )
+// {
+//     result += numbs[i]
+
+
+// }
+numbs.forEach(nums => result += nums)
+return result
+
+}
+/// to convert boolean  into number just multiple it or add plus or nagetive sign
+console.log(addAll(1,3,6,5,5, -true * 1))
+
+
+
+//learn Typescript In Arabic 2022 - #11 - Type Annotations With Anonymous And Arrow Function
+
+
