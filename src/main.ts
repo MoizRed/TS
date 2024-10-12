bad74f60695364b4de37e99d8ed3d6983c514c26
MoizRed <abdoureduniv2000@gmail.com>
2024-10-12 14:22:27 +0100

    some changed

--- src/main.ts
+++ src/main.ts
@@ -80,3 +80,62 @@ if (root) {
     console.error("root element not fodund");
 }
 
+// Learn Typescript In Arabic 2022 - #13 - Data Types - Type Alias Advanced
+
+type buttons = {
+    up: string,
+    right : string,
+    down : string ,
+    left : string
+
+
+}
+//take the same type and add for it for the last
+
+type last = buttons & {
+
+    x:boolean
+}
+
+function Getactions(btns : last) {
+
+    console.log(`action for button up is ${btns.up}`)
+
+    console.log(`action for button right is ${btns.right}`)
+
+    console.log(`action for button down is ${btns.down}`)
+
+    console.log(`action for button left is ${btns.left}`)
+
+
+}
+
+Getactions({
+    up : "up arrow",
+    right : "right arrow",
+    down : "down arrow",
+    left : "left arrow",
+    x: true
+})
+
+
+// Learn Typescript In Arabic 2022 - #14 - Data Types - Literal Types
+type nums = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+function compare (num1:number , num2 : number) : 1 | -1 | 0 {
+
+    return num1 - num2
+
+
+}
+
+// Learn Typescript In Arabic 2022 - #15 - Data Types - Tuple
+
+/*
+
+Tuples is  an array with fixed LENGTH and types
+*/
+
+
+let Article : [number , string , boolean] = [1 , "TITLE" , true ]
+
+Article[1] = "hello"
