fafff057390244c559ae146efa7e0b9f08f5fd5c
MoizRed <abdoureduniv2000@gmail.com>
2024-10-18 21:26:14 +0100

    selectors

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,21 @@
-// //default value of type script
-// function showdata(name: string | null = "guest", age? : number , country : string = "algera" ) : string{
-//     return `${name}  - ${age}  - ${country}`
-// }
-
 import { clear } from "console";
 import { mkdir } from "fs";
 
-// console.log(showdata(null,987,"hi"))
-
-//Learn Typescript In Arabic 2022 - #10 - Function Rest Parameter
-
-function addAll(...numbs: number[]) {
+function addAll(...numbs: number[]): number {
   let result = 0;
-  //     for (let i = 0 ; i < numbs.length ; i ++ )
-  // {
-  //     result += numbs[i]
-
-  // }
   numbs.forEach((nums) => (result += nums));
   return result;
 }
-/// to convert boolean  into number just multiple it or add plus or nagetive sign
-console.log(addAll(1, 3, 6, 5, 5, -true * 1));
 
-//learn Typescript In Arabic 2022 - #11 - Type Annotations With Anonymous And Arrow Function
-
-/*
-FUNCTION
-- anonymous functoin
--arrow function
-*/
+console.log(addAll(1, 3, 6, 5, 5, -Number(true) * 1));
 
 const add = function (num1: number, num2: number): number {
   return num1 + num2;
 };
 
-const hello = (num1: number, num2: number): number => {
-  return num1 + num2;
-};
+const hello = (num1: number, num2: number): number => num1 + num2;
 
 console.log(hello(1, 2));
-
-// Learn Typescript In Arabic 2022 - #12 - Data Types - Type Alias
-
-// data alias
-//type alias
 
 type job = string;
 
@@ -58,27 +29,21 @@ hi = 3;
 
 console.log(hi);
 
-// Learn Typescript In Arabic 2022 - #13 - Data Types - Type Alias Advanced
-
 type buttons = {
   up: string;
   right: string;
   down: string;
   left: string;
 };
-//take the same type and add for it for the last
 
 type last = buttons & {
   x: boolean;
 };
 
-function Getactions(btns: last) {
+function Getactions(btns: last): void {
   console.log(`action for button up is ${btns.up}`);
-
   console.log(`action for button right is ${btns.right}`);
-
   console.log(`action for button down is ${btns.down}`);
-
   console.log(`action for button left is ${btns.left}`);
 }
 
@@ -90,207 +55,108 @@ Getactions({
   x: true,
 });
 
-// Learn Typescript In Arabic 2022 - #14 - Data Types - Literal Types
 type nums = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-function compare(num1: number, num2: number) {
+
+function compare(num1: number, num2: number): number {
   if (num1 > num2) {
     return 1;
   }
 
-  // Learn Typescript In Arabic 2022 - #15 - Data Types - Tuple
-
-  /*
-    
-    Tuples is  an array with fixed LENGTH and types
-    */
-
   let Article: [number, string, boolean] = [1, "TITLE", true];
-
   Article[1] = "hello";
 
-  ///Learn Typescript In Arabic 2022 - #16 - Data Types - Void And Never
-
-  // void functions never return anything
-  // void functions will return undefined
-  //RETURN TYPE NEVER RETURNS
-  //IT THROWS AN ERROR OR NEVER FINISHES RUNNING AT ALL "INFINITE LOOP "
-
-  function show(msg: string) {
+  function show(msg: string): void {
     while (true) {
       console.log(msg);
     }
-    return;
   }
 }
 
-// 0:14 / 5:01
-// Learn Typescript In Arabic 2022 - #17 - Data Types - Enums Part 1
-//done
+let age: number = 38;
+let country: string = "algeria";
 
-
-
-let age : number = 38
-let country : string = "algeria"
-
-
-interface  subject_model {
-
-    name : string
-    age : number
-    country : string
-
-
+interface SubjectModel {
+  name: string;
+  age: number;
+  country: string;
 }
 
-let subject1 : subject_model = {
-    name : "hi",
-    age : 389,
-    country : "agleria"
+let subject1: SubjectModel = {
+  name: "hi",
+  age: 389,
+  country: "algeria",
+};
 
+type Document = [string, number, boolean];
 
+let doc1: Document = ["classified", 2, false];
 
+let fridge: "salad" | "tomato" = "tomato";
+
+let img = <HTMLElement>document.getElementById("img") as HTMLInputElement;
+console.log(img?.value);
+
+let data: any = "1000";
+console.log((data as string).length);
+
+interface User {
+  readonly name: string;
+  age?: number;
+  country: string;
+  sayhello(): string;
 }
 
-//tuple
-type __document = [string, number, boolean] ;
-
-
-let doc1 : __document = ["classified", 2 , false]
-
-
-//litral type is easy 
-
-
-let fridge : "salad" | "tomato"
-
-
-fridge = "tomato"
-
-
- 
-//Learn Typescript In Arabic 2022 - #19 - Data Types - type assertion
-
-
-let img = <HTMLElement> document.getElementById("img") as HTMLInputElement
- 
-console.log(img.value);
-
-let data :  any = "1000"
-
-console.log((data as string).length)
-
-
-//Learn Typescript In Arabic 2022 - #20 - union types intersection types
-
-
-
-//Learn Typescript In Arabic 2022 - #21 - type annotations with objects
-
-
-
-//Learn Typescript In Arabic 2022 - #22 - interface decliration
-
-
-
-
-interface user {
-  readonly name : string,
-  age? : number,
-   country : string,
-   sayhello() : string
-
-   }
-
-
-
-let user1 : user ={
-
-  name : "hi",
-  country : "agleria",
+let user1: User = {
+  name: "hi",
+  country: "agleria",
   sayhello() {
-      return "helo"
+    return "helo";
   },
+};
+
+
+user1.country = "hello";
+
+interface Settings {
+  theme: boolean;
+  font: string;
 }
 
-user1 .country = "hello"
-
-// Learn Typescript In Arabic 2022 - #23 - Interface Method And Parameters
-
-
-// Learn Typescript In Arabic 2022 - #24 - Interface Reopen And Use Cases
-
-
-interface settings {
-
-  theme : boolean;
-  font : string ;
-
-
+interface Users {
+  id: number;
+  username: string;
+  country: string;
 }
 
-// Learn Typescript In Arabic 2022 - #25 - Interface Extend
-
-interface users{
-  id : number ; 
-  username: string ;
-  country : string
-
+interface Moderator extends Users {
+  edit: boolean;
 }
 
-
-interface moderator extends users {
-
-  edit : boolean;
-
-
-
+interface Admin extends Moderator {
+  delete: boolean;
 }
 
+let user: Admin = {
+  id: 3,
+  username: "capous",
+  country: "alaroda",
+  edit: false,
+  delete: false,
+};
 
-interface admin extends moderator {
-  delete : boolean;
-
+export function returntype<T>(val: T): T {
+  return val;
 }
-
-let user : admin = {
-
-    id : 3,
-    username : "capous",
-    country: "alaroda",
-    edit : false,
-    delete : false,
+class Users<T> {
+  private value: T;
 
 
-}
-
-// Learn Typescript In Arabic 2022 - #26 - Interface Final Discussion
-
-
-//Learn Typescript In Arabic 2022 - #27 - Class Type Annotations
-
-// Learn Typescript In Arabic 2022 - #28 - Class Access Modifiers And Parameters Properties
-// Learn Typescript In Arabic 2022 - #30 - Class Static Members
-// Learn Typescript In Arabic 2022 - #31 - Class Implements interface
-
-// Learn Typescript In Arabic 2022 - #32 - Abstract Classes And Members 
-
-// Learn Typescript In Arabic 2022 - #33 - Polymorphism And Method Override
-// Learn Typescript In Arabic 2022 - #34 - Generics Introduction
-// Learn Typescript In Arabic 2022 - #35 - Generics Multiple Types
-// Learn Typescript In Arabic 2022 - #37 - generic classd
-// Learn Typescript In Arabic 2022 - #37 - Generics And Interfaces
-
-
-
-export function returntype<T>(val : T) : T {
-
-  return val
-}
-
-class users<T> {
-  constructor(value : T)  {
-    
+  constructor(value: T) {
+    this.value = value;
   }
-  showmsg(msg : T) : string {
-    
+
+  showmsg(msg: T): string {
+    return msg;
   }
+}
+
